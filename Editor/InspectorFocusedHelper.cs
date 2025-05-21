2890b2e9e7ed841b48d7bd195b2d99ba595f5dc2
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-21 12:45:54 +0200

    ⠀⠀⠀⠀⠀

---
+++ Editor/InspectorFocusedHelper.cs
@@ -0,0 +1,30 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityEssentials
+{
+    public static class InspectorFocusedHelper
+    {
+        public static bool ProcessKeyboardClick(Rect position)
+        {
+            var controlId = GetControlId(position);
+            var isControlFocused = GUIUtility.keyboardControl == controlId;
+
+            if (isControlFocused && Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return)
+            {
+                Event.current.Use();
+                return true;
+            }
+
+            if (isControlFocused)
+                GUI.DrawTexture(position, EditorGUIUtility.whiteTexture, ScaleMode.StretchToFill, false, 0, new Color(0.25f, 0.5f, 1f, 1f), 1, 1);
+
+            return false;
+        }
+
+        public static int GetControlId(Rect position) =>
+            GUIUtility.GetControlID(FocusType.Keyboard, position);
+    }
+}
+#endif
