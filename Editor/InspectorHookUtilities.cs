ab0e92ec05b1ec39f69ea1b1977b58c49dca3fb4
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-21 03:57:59 +0200

    ⠀⠀⠀⠀⠀

--- Editor/InspectorHookUtilities.cs
+++ Editor/InspectorHookUtilities.cs
@@ -167,6 +167,35 @@ namespace UnityEssentials
 
             return fieldInfo;
         }
+
+        public static object GetPropertyValue(SerializedProperty property) =>
+            property.propertyType switch
+            {
+                SerializedPropertyType.Integer => property.intValue,
+                SerializedPropertyType.Boolean => property.boolValue,
+                SerializedPropertyType.Float => property.floatValue,
+                SerializedPropertyType.String => property.stringValue,
+                SerializedPropertyType.Color => property.colorValue,
+                SerializedPropertyType.ObjectReference => property.objectReferenceValue,
+                SerializedPropertyType.LayerMask => property.intValue,
+                SerializedPropertyType.Enum => property.enumValueIndex,
+                SerializedPropertyType.Vector2 => property.vector2Value,
+                SerializedPropertyType.Vector3 => property.vector3Value,
+                SerializedPropertyType.Vector4 => property.vector4Value,
+                SerializedPropertyType.Rect => property.rectValue,
+                SerializedPropertyType.ArraySize => property.arraySize,
+                SerializedPropertyType.Character => (char)property.intValue,
+                SerializedPropertyType.AnimationCurve => property.animationCurveValue,
+                SerializedPropertyType.Bounds => property.boundsValue,
+                SerializedPropertyType.Quaternion => property.quaternionValue,
+                SerializedPropertyType.ExposedReference => property.exposedReferenceValue,
+                SerializedPropertyType.Vector2Int => property.vector2IntValue,
+                SerializedPropertyType.Vector3Int => property.vector3IntValue,
+                SerializedPropertyType.RectInt => property.rectIntValue,
+                SerializedPropertyType.BoundsInt => property.boundsIntValue,
+                SerializedPropertyType.ManagedReference => property.managedReferenceValue,
+                _ => null
+            };
     }
 }
 #endif
